74abc890f2f1088acdd69119f12bc9d9614e86e0
silkworm2910 <silkworm2910@gmail.com>
2018-10-17 19:39:11 +0300

    123

---
+++ src/Lesson1.java
@@ -0,0 +1,10 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+//однострочный комментарий ctrl/
+        /*
+        многострочный комментарий ctrl+shift/
+         */
+//sout
+        System.out.println("Lesson1");
+    }
+}
