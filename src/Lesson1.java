f453ed4b2ba8fc7cf6c68a5eb88245fc2d7d8f39
silkworm2910 <silkworm2910@gmail.com>
2018-10-17 21:38:41 +0300

    123commit

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,10 +1,23 @@
 public class Lesson1 {
     public static void main(String[] args) {
+//        psvm
 //однострочный комментарий ctrl/
         /*
         многострочный комментарий ctrl+shift/
          */
 //sout
         System.out.println("Lesson1");
+        //объявление переменных
+//        типДанных имяПеременной;
+        // имя переменной со строчного символа, символы, цифры, нп.
+        //имяПеременной = значение;
+        byte byteVar;
+        byteVar=1;
+        short shortVar;
+        shortVar=90;
+        //явное приведение типов
+        int intNum=23;
+        byte byteNum = (byte) intNum;
+
     }
 }
