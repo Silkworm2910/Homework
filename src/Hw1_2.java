a889fe95c87ed424b16809edfb811b5717551d14
silkworm2910 <silkworm2910@gmail.com>
2018-10-19 03:25:27 +0300

    task 5

---
+++ src/Hw1_2.java
@@ -0,0 +1,5 @@
+public class Hw1_2 {
+    public static void main(String[] args) {
+        System.out.println(10*10*10-25*15);
+    }
+}
---
+++ src/Hw1_4.java
@@ -0,0 +1,15 @@
+public class Hw1_4 {
+    public static void main(String[] args) {
+        float m=16.7F;
+        float n=12.45F;
+        float x=Math.abs(10-m);
+        float y=Math.abs(10-n);
+            if (x<y) {System.out.println(m);
+            }
+            else {
+                System.out.println(n);
+
+            }
+
+    }
+}
